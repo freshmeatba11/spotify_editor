f760f9d9908c6a498422697f7cf36f9abe7cfc37
Jason Chen <cmjj8898212032@gmail.com>
2023-11-24 17:13:01 +0800

    feat: add tabs in sidemenu.

--- src/components/sideMenu/sideMenuBottom.tsx
+++ src/components/sideMenu/sideMenuBottom.tsx
@@ -2,6 +2,7 @@ import clsx from "clsx";
 import { Disc3, Plus } from "lucide-react";
 
 import { Button } from "../ui/button";
+import PlaylistWithTabs from "./playlistWithTabs";
 
 const LibraryButton = ({ onClick }: { onClick: () => void }) => {
   return (
@@ -22,15 +23,19 @@ const LibraryButton = ({ onClick }: { onClick: () => void }) => {
 const PlusButton = () => {
   return (
     <Button
-      onClick={() => {}}
+      onClick={() => {
+        alert("plus");
+      }}
       className={clsx(
         "p-2",
         "rounded-full",
         "text-[var(--sideMenu-text)] text-base hover:text-[var(--sideMenu-text-hover)]",
-        "bg-transparent hover:bg-transparent"
+        "bg-transparent hover:bg-transparent",
+        "@[75px]/sidemenu:block",
+        "hidden"
       )}
     >
-      <Plus className={clsx("h-6 w-6", "@[75px]/sidemenu:block", "hidden")} />
+      <Plus className={clsx("h-6 w-6")} />
     </Button>
   );
 };
@@ -41,16 +46,18 @@ type Props = {
 const SideMenuBottom = ({ setIsSideMenuOpen }: Props) => {
   const onClickLibrary = () => setIsSideMenuOpen((prev) => !prev);
   return (
-    <div
-      className={clsx(
-        "rounded-lg",
-        "flex flex-col gap-2",
-        "bg-[var(--sideMenu-bg)]"
-      )}
-    >
+    <div className={clsx("rounded-lg", "bg-[var(--sideMenu-bg)]")}>
       <div className={clsx("py-2 px-4", "flex gap-2 justify-between")}>
         <LibraryButton {...{ onClick: onClickLibrary }} />
         <PlusButton />
+      </div>
+      <div
+        className={clsx(
+          "pt-0 px-1 pb-1",
+          "@[75px]/sidemenu:py-2 @[75px]/sidemenu:px-4"
+        )}
+      >
+        <PlaylistWithTabs />
       </div>
     </div>
   );
